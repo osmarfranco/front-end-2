f6cabd398b13cf988a0c8d9af74a6c1ef9725e57
Osmar Franco <osmarfranco0@gmail.com>
2022-06-22 18:16:39 -0300

    Refatoração aplicando async/await e separando cada função de acordo com seu papel no código

--- Mesa 17/js/index.js
+++ Mesa 17/js/index.js
@@ -1,24 +1,22 @@
 // Aqui realizamos a consulta da promise, esperando sua resposta assíncrona
-function requisitarERenderizarDados(){
-    fetch('https://randomuser.me/api/')
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        let user = data.results[0]
-        renderizarDadosUsuario(user)
-    });
+async function requisitarDados() {
+    const data = await fetch('https://randomuser.me/api/')
+    const json = await data.json()
+
+    return json.results[0]
 }
 
 //Elementos HTML
 const CARD = document.getElementById("card")
 const RANDOM_BTN = document.getElementById("random")
 
-function renderizarDadosUsuario(user) {
+async function renderizarDadosUsuario() {
     /* -------------------------------- Tarefa 1 -------------------------------- */
     // Aqui devem desenvolver uma função que seja exibida na tela:
     // a foto, o nome completo do usuário e o e-mail.
     // Isto deve ser baseado nas informações que obtemos da API e inseridas no HTML.
+    let user = await requisitarDados()
+    
     let html = 
     `<img src="${user.picture.large}">
     <h3>${user.name.title} ${user.name.first} ${user.name.last}</h3>
@@ -33,9 +31,9 @@ function renderizarDadosUsuario(user) {
 // Cabe aos desenvolvedores decidirem qual bloco de código deve ser contido dentro de uma função para que ele possa ser executado toda vez que um clique de botão for realizado.
 
 RANDOM_BTN.addEventListener("click", () => {
-    requisitarERenderizarDados()
+    renderizarDadosUsuario()
 })
 
 onload = () => {
-    requisitarERenderizarDados()
+    renderizarDadosUsuario()
 }
